feb24c12d019387322db2e151fad4f0715feb1f4
Andrew Yip <yipcma@gmail.com>
2018-01-04 22:42:52 +0800

    add space and plot agent counts

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+from mesa.space import MultiGrid
 import random
 
 class MoneyAgent(Agent):
@@ -11,21 +12,41 @@ class MoneyAgent(Agent):
     def step(self):
         # The agent's step will go here
         # print(self.unique_id)
-        if self.wealth == 0:
-            return
-        other_agent = random.choice(self.model.schedule.agents)
-        other_agent.wealth += 1
-        self.wealth -= 1
+        self.move()
+        if self.wealth > 0:
+            self.give_money()
 
+    
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False
+        )
+        new_position = random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def give_money(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            other_agent = random.choice(cellmates)
+            other_agent.wealth += 1
+            self.wealth -= 1
+            
 class MoneyModel(Model):
     """A model with some number of agents."""
-    def __init__(self, N):
+    def __init__(self, N, width, height):
         self.num_agents = N
         self.schedule = RandomActivation(self)
+        self.grid = MultiGrid(width, height, True)
         # Create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
             self.schedule.add(a)
+            # add the agent to a random grid cell
+            x = random.randrange(self.grid.width)
+            y = random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
     
     def step(self):
         '''Advance the model by one step.'''
--- run.py
+++ run.py
@@ -1,14 +1,24 @@
 from model import *
 import matplotlib.pyplot as plt
+import numpy as np
 
 all_wealth = []
-for j in range(100):
-    model = MoneyModel(10)
-    for i in range(10):
-        model.step()
-    agent_wealth = [a.wealth for a in model.schedule.agents]
 
-    all_wealth.extend(agent_wealth)
+model = MoneyModel(50, 10, 10)
+for i in range(20):
+    model.step()
+agent_wealth = [a.wealth for a in model.schedule.agents]
+agent_counts = np.zeros((model.grid.width, model.grid.height))
+for cell in model.grid.coord_iter():
+    cell_content, x, y = cell
+    agent_count = len(cell_content)
+    agent_counts[x][y] = agent_count
+plt.imshow(agent_counts, interpolation='nearest')
+plt.colorbar()
+
+# If running from a text editor or IDE, remember you'll need the following:
+plt.show()
+all_wealth.extend(agent_wealth)
 
 plt.hist(all_wealth)
 plt.show()
