2485f05d1875a72864cd6fd57ed204f12a632f47
Andrew Yip <yipcma@gmail.com>
2018-01-05 11:24:45 +0800

    customize agent appearance

--- server.py
+++ server.py
@@ -3,7 +3,15 @@ from mesa.visualization.ModularVisualization import ModularServer
 from model import MoneyModel
 
 def agent_portrayal(agent):
-    portrayal = {"Shape": "circle", "Color": "red", "Filled": "true", "Layer": 0, "r": 0.5}
+    portrayal = {"Shape": "circle", "Filled": "true", "r": 0.5}
+
+    if agent.wealth > 0:
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = 0
+    else:
+        portrayal["Color"] = "grey"
+        portrayal["Layer"] = 1
+        portrayal["r"] = 0.2
     return portrayal
 
 grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
